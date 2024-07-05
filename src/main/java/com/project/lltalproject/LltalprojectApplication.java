197abc59c946b8a6eb192ca457f756d676654a2c
lpw0625 <dlvuddnjsss@gmail.com>
2024-07-05 20:55:56 +0900

    일팔 프로젝트 백앤드

---
+++ src/main/java/com/project/lltalproject/LltalprojectApplication.java
@@ -0,0 +1,13 @@
+package com.project.lltalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LltalprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LltalprojectApplication.class, args);
+	}
+
+}
